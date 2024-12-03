0db7bf6195c0106883b6e1cdf011be3f79d4bd4d
Steven Veld <sveld03@gmail.com>
2024-12-02 16:44:53 -0800

    check_canonicity_many works with parallelized batch implementation.

--- check_canonicity_streamlined.py
+++ check_canonicity_streamlined.py
@@ -36,69 +36,75 @@ def check_canonicity_percent(actual_tokens, canonical_tokens):
 
 
 def check_canonicity_many():
-    output_file = "canonicity_survey_test_1.txt"
+    output_file = "12-2-batch-check-canonicity.txt"
+    raw_file = "12-2-batch-raw-check-canonicity.txt"
 
-    raw_file = "canonicity_raw_test_1.txt"
+    device = torch.device("cuda:0")
 
     token_counts = [1, 50, 100, 200, 300, 500]
     step_counts = [100, 100, 100, 100, 100, 100]
 
-    groups = 2 # change to 6
-    batches_per_group = 3 # change to 100
+    batches = 2 # change to 6
+    batch_size = 3 # change to 100
     
-    # for i in range(groups): 
-    #     token_count = token_counts[i]
+    for i in range(batches): 
+        token_count = token_counts[i]
+        steps = step_counts[i]
+
+        actual_tokens = sample_tokens(batch_size, token_count, steps)
+        text = tokenizer.batch_decode(actual_tokens)
+        canonical_tokens = tokenizer(
+            text, 
+            padding=False, 
+            truncation=True,
+            return_tensors='pt')["input_ids"].to(device)
+
+        canon_count = 0
+
+        for j in range(batch_size):
+            actual_sample = actual_tokens[j]
+            canonical_sample = canonical_tokens[j]
+            text_sample = text[j]
+            canon = check_canonicity_bool(actual_sample, canonical_sample)
+            with open(raw_file, 'a') as file:
+                file.write("For iteration " + str(j) + " of " + str(token_count) + " tokens and " + str(steps) + " steps, here were the results:\n\n")
+                file.write("Canonical? " + ("Yes\n" if canon else "No\n"))
+                file.write("Actual tokens:\n" + str(actual_sample) + "\n\n")
+                file.write("Canonical tokens:\n" + str(canonical_sample) + "\n\n")
+                file.write("Text:\n" + text_sample + "\n\n")
+                file.write("=================================================================\n\n\n")
+            if canon:
+                canon_count += 1
+        
+        percent = (canon_count / batch_size) * 100
+
+        with open(output_file, 'a') as file:
+            file.write("For " + str(token_count) + " tokens and " + str(steps) + " steps, percent canonicity was " + str(percent) + "%\n")
+
+
+    # for i in range(batches): 
+    #     tokens = token_counts[i]
     #     steps = step_counts[i]
-
-    #     actual_tokens = sample_tokens(batches_per_group, token_count, steps) # change to 100
-    #     text = tokenizer.batch_decode(actual_tokens)
-    #     canonical_tokens = tokenizer.batch_encode(text)
-
     #     canon_count = 0
-
-    #     for j in range(batches_per_group): # change to 100
-    #         actual_batch = actual_tokens[j]
-    #         canonical_batch = canonical_tokens[j]
-    #         canon = check_canonicity_bool(actual_batch, canonical_batch)
+    #     for j in range(batch_size):
+    #         actual_tokens_raw = sample_tokens(1, tokens, steps)
+    #         actual_tokens = actual_tokens_raw.squeeze(0)
+    #         text = tokenizer.decode(actual_tokens)
+    #         canonical_tokens_list = tokenizer.encode(text)
+    #         canonical_tokens = torch.tensor(canonical_tokens_list, device='cuda:0')
+    #         canon = check_canonicity_bool(actual_tokens, canonical_tokens)
     #         with open(raw_file, 'a') as file:
-    #             file.write("For iteration " + str(j) + " of " + str(token_count) + " tokens and " + str(steps) + " steps, here were the results:\n\n")
-    #             file.write("Canonical? " + ("Yes" if canon else "no"))
+    #             file.write("For iteration " + str(j) + " of " + str(tokens) + " tokens and " + str(steps) + " steps, here were the results:\n\n")
     #             file.write("Actual tokens:\n" + str(actual_tokens) + "\n\n")
     #             file.write("Canonical tokens:\n" + str(canonical_tokens) + "\n\n")
     #             file.write("Text:\n" + text + "\n\n")
     #             file.write("=================================================================\n\n\n")
     #         if canon:
     #             canon_count += 1
-        
-    #     percent = (canon_count / (groups * batches_per_group)) * 100
+    #     percent_canonicity = (canon_count / batch_size) * 100
 
     #     with open(output_file, 'a') as file:
-    #         file.write("For " + str(token_count) + " tokens and " + str(steps) + " steps, percent canonicity was " + str(percent) + "%\n")
-
-
-    for i in range(2): 
-        tokens = token_counts[i]
-        steps = step_counts[i]
-        canon_count = 0
-        for j in range(3):
-            actual_tokens_raw = sample_tokens(1, tokens, steps)
-            actual_tokens = actual_tokens_raw.squeeze(0)
-            text = tokenizer.decode(actual_tokens)
-            canonical_tokens_list = tokenizer.encode(text)
-            canonical_tokens = torch.tensor(canonical_tokens_list, device='cuda:0')
-            canon = check_canonicity_bool(actual_tokens, canonical_tokens)
-            with open(raw_file, 'a') as file:
-                file.write("For iteration " + str(j) + " of " + str(tokens) + " tokens and " + str(steps) + " steps, here were the results:\n\n")
-                file.write("Actual tokens:\n" + str(actual_tokens) + "\n\n")
-                file.write("Canonical tokens:\n" + str(canonical_tokens) + "\n\n")
-                file.write("Text:\n" + text + "\n\n")
-                file.write("=================================================================\n\n\n")
-            if canon:
-                canon_count += 1
-        percent_canonicity = canon_count
-
-        with open(output_file, 'a') as file:
-            file.write("For " + str(tokens) + " tokens and " + str(steps) + " steps, percent canonicity was " + str(percent_canonicity) + "%\n")
+    #         file.write("For " + str(tokens) + " tokens and " + str(steps) + " steps, percent canonicity was " + str(percent_canonicity) + "%\n")
 
 def main():
     check_canonicity_many()
